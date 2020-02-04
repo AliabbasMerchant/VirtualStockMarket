ab9a3f8e0af695e5e150548f9370a833d7acea7a
AliabbasMerchant <merchantaliabbas@gmail.com>
2020-02-04 16:52:16 +0530

    VSM Page Done

--- client/src/components/VSM.js
+++ client/src/components/VSM.js
@@ -1,47 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fundsChange } from '../reducers/funds';
 
-const VSM = ({ funds, fundsChange }) => {
+import StocksListElement from './StocksListElement';
+
+const VSM = ({ stocks }) => {
     return (
-        <div className="mx-0 center container" style={{ width: '100%', maxWidth: '100%' }}>
-            VSM
-            <div>Funds: {funds}</div>
-            <div onClick={() => { fundsChange(50); }}>Change by 50</div>
-        </div>
+        stocks.length ?
+            <div className="mx-0 center container" style={{ width: '100%', maxWidth: '100%' }}>
+                <table className="row">
+                    <tbody>
+                        {stocks.map((stock, stockIndex) => {
+                            return <StocksListElement key={stockIndex} data={stock} id={stockIndex} />
+                        })}
+                    </tbody>
+                </table>
+            </div> :
+            <div className="center container">No stocks</div>
     );
 };
 
 const mapStateToProps = (state) => ({
-    funds: state.funds
+    stocks: state.stocks
 });
-
-const mapDispatchToProps = { fundsChange };
 
 export default connect(
     mapStateToProps,
-    mapDispatchToProps
+    null
 )(VSM);
-
-
-// import { StocksContext } from '../contexts/stocks';
-// import StocksListElement from './StocksListElement';
-// function VSM() {
-//     return (
-//         <StocksContext.Consumer>
-//             {(stocksContext) =>
-//                 stocksContext.stocks ?
-//                     <div className="mx-0 center container" style={{ width: '100%', maxWidth: '100%' }}>
-//                         <table className="row">
-//                             <tbody>
-//                                 {stocksContext.stocks.map((stock, stockIndex) => {
-//                                     return <StocksListElement key={stockIndex} data={stock} id={stockIndex} />
-//                                 })}
-//                             </tbody>
-//                         </table>
-//                     </div> :
-//                     <div className="center container">No stocks</div>
-//             }
-//         </StocksContext.Consumer>
-//     );
-// }
